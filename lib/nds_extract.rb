5c159059d4edc27a59834f2e16b668dd0de4f2ee
Paul Kim <pkim1295@gmail.com>
2020-01-16 03:13:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,7 @@ require 'pp'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
+pp directors_database
 def gross_for_director(director_data)
   gross = 0
   
